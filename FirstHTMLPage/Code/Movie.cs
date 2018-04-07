5e0f651993f4d63f68f639bc93f7e5ac920155bc
Dmitry Manakhov <dmanakho@Hotmail.com>
2018-04-07 18:18:06 -0400

    Initial commit

---
+++ FirstHTMLPage/Code/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstHTMLPage.Code
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Cast { get; set; }
+
+        public Movie()
+        { }
+        public Movie(string title, string director, int releaseYear, string cast)
+        {
+            Title = title;
+            Director = director;
+            ReleaseYear = releaseYear;
+            Cast = cast;
+        }
+    }
+}
